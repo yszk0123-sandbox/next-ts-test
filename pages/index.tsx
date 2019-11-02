f245be76172d3f398e8b8caecf5019f10cb58a99
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-11-02 17:37:54 +0900

    Add typings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,21 @@
-const Home = () => <h1>Hello, world!</h1>;
+import { NextPage } from "next";
+
+interface Props {
+  userAgent: string;
+}
+
+const Home: NextPage<Props> = ({ userAgent }) => {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <p>user agent: {userAgent}</p>
+    </>
+  );
+};
+
+Home.getInitialProps = async ({ req }) => {
+  const userAgent = req ? req.headers["user-agent"] || "" : navigator.userAgent;
+  return { userAgent };
+};
 
 export default Home;
